ddd8097c9ccc983f8f190143b2dee8ce14fb7b4a
Sarah Alsharif <197163@ppu.edu.ps>
2021-11-15 17:57:44 +0200

    edit the header design

--- autocomplete/src/lib.js
+++ autocomplete/src/lib.js
@@ -1,4 +1,3 @@
 Event.observe(window, 'load', function() {
         new AutoComplete('articleTitle', 'autocomplete.xqy?q=', { delay: 0.1 });
-        new AutoComplete('authorLastName', 'autocomplete.xqy?q=', { delay: 0.1 });
         });
